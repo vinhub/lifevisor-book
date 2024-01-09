5809165af0effcbe40c77bac6d7c9b8633613a0f
Vinayak (Vin) Bhalerao <vin.bhalerao@gmail.com>
2024-01-09 12:39:24 -0800

    started building pdf and epub files

---
+++ build.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+rm -rf _working
+mkdir _working
+cp -a raw/. _working/
+cp title.txt _working
+cd _working
+
+# convert images specified as <figure> tags to normal markdown images
+perl -i -0pe 's/<figure\b[^>]*><img\b(?:[^>]*?\s)?src\=\"([^\"]*)\"[^<]*<figcaption><p>([^<]*)<\/p><\/figcaption><\/figure>/!\[\2\](\<\1\>)/mg' *.md
+
+# issues: links not working, table formatting, smiley face in font, styling
+
+# Convert Markdown to PDF with custom styling
+pandoc -o book.pdf *.md \
+  --toc \
+  --pdf-engine=xelatex \
+  --template=../pubtools/eisvogel.latex \
+  --variable mainfont="Arial Unicode MS" \
+  --include-before-body ../pubtools/cover.tex \
+  --metadata=title:"An Engineer's Search for Meaning" \
+  --metadata=author:"Vinayak (Vin) Bhalerao" \
+  --metadata=lang:"en-US" \
+  --listings
+
+# Convert Markdown to ePub with custom styling
+pandoc -o book.epub *.md \
+  --toc \
+  --epub-cover-image=.GitBook/assets/cover.png \
+  --variable mainfont="Arial Unicode MS" \
+  --metadata=title:"An Engineer's Search for Meaning" \
+  --metadata=author:"Vinayak (Vin) Bhalerao" \
+  --metadata=lang:"en-US" \
+  --css ../pubtools/epub.css
