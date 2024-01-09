05a0a73be4c182f24a3f3b000a0bea4f7c8aa542
Vinayak (Vin) Bhalerao <vin.bhalerao@gmail.com>
2024-01-09 13:58:58 -0800

    removed extraneous content

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 rm -rf _working
 mkdir _working
 cp -a raw/. _working/
+rm -f _working/README.md
+rm -f _working/SUMMARY.md
 cp title.txt _working
 cd _working
 
 # convert images specified as <figure> tags to normal markdown images
 perl -i -0pe 's/<figure\b[^>]*><img\b(?:[^>]*?\s)?src\=\"([^\"]*)\"[^<]*<figcaption><p>([^<]*)<\/p><\/figcaption><\/figure>/!\[\2\](\<\1\>)/mg' *.md
 
-# issues: links not working, table formatting, smiley face in font, styling
+# issues: links not working, table formatting, styling
 
 # Convert Markdown to PDF with custom styling
 pandoc -o book.pdf *.md \
