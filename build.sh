adfa53f4d685fa6977ad22f1b7e461ace1fde791
Vinayak (Vin) Bhalerao <vin.bhalerao@gmail.com>
2024-01-10 14:38:04 -0800

    layout fixes

--- build.sh
+++ build.sh
@@ -10,20 +10,29 @@ cd _working
 # convert images specified as <figure> tags to normal markdown images
 perl -i -0pe 's/<figure\b[^>]*><img\b(?:[^>]*?\s)?src\=\"([^\"]*)\"[^<]*<figcaption><p>([^<]*)<\/p><\/figcaption><\/figure>/!\[\2\](\<\1\>)/mg' *.md
 
+# Use git to get creation and modification date
+lastUpdated=$( git log -1 --format="%ci" -- $fullpath | cut -f 1 -d ' ' )
+creationDate=$( git log --format="%ai" -- $fullpath | tail -1 | cut -f 1 -d ' ' )
+
 # issues: epub toc is broken, links not working, styling
 
 # Convert Markdown to PDF with custom styling
 pandoc -o book.pdf title.txt *.md \
   --toc \
   --pdf-engine=xelatex \
-  --template=../pubtools/eisvogel.latex \
-  --variable mainfont="Arial Unicode MS" \
+  -V geometry:margin=1.01in \
+  --top-level-division=chapter \
+  --variable=lastUpdated:$lastUpdated \
+  --variable=creationDate:$creationDate \
   --include-before-body ../pubtools/cover.tex \
   --listings
 
 # Convert Markdown to ePub with custom styling
 pandoc -o book.epub title.txt *.md \
   --toc \
+  -V geometry:margin=1in \
+  --top-level-division=chapter \
+  --variable=lastUpdated:$lastUpdated \
+  --variable=creationDate:$creationDate \
   --epub-cover-image=.GitBook/assets/cover.png \
-  --variable mainfont="Arial Unicode MS" \
   --css ../pubtools/epub.css
